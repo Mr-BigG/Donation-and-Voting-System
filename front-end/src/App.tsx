00aef0b317cb8686141a19362ccd2c05a1e45cf9
Mr-BigG <1919326439@qq.com>
2024-10-03 18:24:59 +0800

    重构前端

---
+++ front-end/src/App.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import React from 'react';
+import './App.css';
+import DonationAndVotingSystemContract from "./pages/DonationAndVotingSystemContract/DonationAndVotingSystemContract"
+
+function App() {
+  return (
+    <div className="App">
+      <DonationAndVotingSystemContract></DonationAndVotingSystemContract>
+    </div>
+  );
+}
+
+export default App;
